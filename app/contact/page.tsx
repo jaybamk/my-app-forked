e18d5dc2c8cccb4f745371907619e94d89d6679e
Brotherbond <ehinmitankehinde@gmail.com>
2024-06-17 21:13:33 +0100

    17_06_2

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "@/app/page.module.scss";
+
+export default function ContactUs() {
+  return (
+    <main className={styles.main}>
+      <div>My Next.js contact page</div>
+    </main>
+  );
+}
