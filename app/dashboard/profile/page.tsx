2b6e1cc012a16c557dbb5dfc92ca7513acb8a9a0
Brotherbond <ehinmitankehinde@gmail.com>
2024-07-01 21:03:44 +0100

    01_07

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,7 +1,22 @@
 import styles from "@/app/page.module.scss";
 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// Create an Iterator
+const list = fruits.entries();
+const newArr = fruits.findLastIndex((value, index, arr) => {
+    // newArr.push(value)
+    console.log(value, index)
+
+
+    return value.charAt(1) === "a"
+    return `${value} has index ${index}`
+})
+console.log(newArr)
+
 export default function Profile() {
     const isLoggedIn = true;
+
     return <>
         <div className={!isLoggedIn ? styles.loggedOut : styles.loggedIn}>
             {isLoggedIn ? <AdminPanel /> : <LoginForm />}
