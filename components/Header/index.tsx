dc76e1c68740bf380d1c2b202ae1470e3e2aa1ee
Brotherbond <ehinmitankehinde@gmail.com>
2024-07-29 20:53:29 +0100

    In progress state

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import styles from './styles.module.scss'
 import Link from "next/link";
 import routes from "@/routes";
+import logo from "@/img/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Header() {
     return <>
@@ -9,14 +11,19 @@ export default function Header() {
             <nav className={`w-100 d-flex justify-content-between`}>
                 <div>
                     <Link href="/">
-                        <Image src="/vercel.svg" alt="logo" width={72} height={16} />
+                        <Image src={logo} alt="logo" width={120} height={45} />
                     </Link>
                 </div>
-                <ul className={`d-flex list-unstyled gap-10`}>
-                    <li><Link href={routes.support.contact}>contact</Link></li>
-                    <li><Link href={routes.support.about}>about</Link></li>
-                    <li><Link href={routes.auth.signin}>signin</Link></li>
-                </ul>
+                <div>
+                    <ul className={`d-none d-md-flex list-unstyled gap-10`}>
+                        <li><Link href={routes.support.contact}>contact</Link></li>
+                        <li><Link href={routes.support.about}>about</Link></li>
+                        <li><Link href={routes.auth.signin}>signin</Link></li>
+                    </ul>
+                    <div>
+                    {/* <FontAwesomeIcon icon="fa-solid fa-bars" /> */}
+                    </div>
+                </div>
             </nav>
         </header>
     </>
