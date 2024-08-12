7b779695a9af272af063f29ebc185e307fdcf450
Brotherbond <ehinmitankehinde@gmail.com>
2024-08-12 20:15:12 +0100

    Fixed Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import routes from "@/routes";
-import hero from "@/img/homepage-section1.png"
+import hero from "@/img/homepage-section11.png"
 import styles from "@/app/page.module.scss";
 import Footer from "@/components/Footer/index";
 import Header from "@/components/Header/index";
@@ -13,8 +13,8 @@ export default function Home() {
   return <>
     <Header />
     <main className={styles.main}>
-      <section className="d-flex flex-column flex-lg-row gap-5" >
-        <div className="p-7vw">
+      <section className="w-100 d-flex flex-column flex-md-row align-items-md-center gap-3 gap-md-0" >
+        <div className="p-7vw col-md-7">
           <h1>Earn over <span className="text-secondary">$500</span> Monthly</h1>
           <h2>
             <div className="text-secondary">LEARN AND EARN</div>
@@ -22,8 +22,9 @@ export default function Home() {
           </h2>
           <p>Our online education platform is a fully functional learning place consisting of virtual class,e-library,testing/assessment modus and others.It is an interactive medium where students can engage the teachers just from the comfort of their homes.</p>
         </div>
-        <div><Image src={hero} alt="hero" width={500} height={500} /></div>
-
+        <div className={`w-100 ${styles.hero}`}>
+          <Image className="w-100" src={hero} alt="hero" />
+        </div>
       </section>
 
 
