7f6fb2baac709642d6c9d35cac0d0550d6ece2d5
djtimog <ogunleyetimilehin14@gmail.com>
2024-08-14 21:15:27 +0100

    changed the location of img folder i also did the backgroung image in signup and editted signin page a little

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -9,8 +9,8 @@ export default function SignIn(){
     return(
     <>
         <div className="row">
-            <div className="d-none d-lg-flex col-lg-5 bg-primary justify-content-around">
-                <Image src="/signin-img.svg" alt="signin image" width={400} height={700}/>
+            <div className="d-none d-lg-flex col-lg-5 bg-primary justify-content-around align-content-center p-5">
+                <Image src="/signin-img.svg" alt="signin image" width={400} height={700} className='w-100'/>
             </div>
             <div className="col-xs-12 col-lg-7 p-1 p-sm-2 p-md-3 p-lg-4 p-xl-5">
                 <Link href="/" >
